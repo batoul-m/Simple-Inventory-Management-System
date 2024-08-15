54b9dfdf7f8814755f8fc1cdf925e27336c8cbac
batoul-m <batoul.2003.m@gmail.com>
2024-08-15 13:01:56 +0300

    Used Pascal Casing as name convention and make the product prop as public then make some changing according to that

--- MangmentSystem/product.cs
+++ MangmentSystem/product.cs
@@ -2,47 +2,18 @@
 namespace MangmentSystem
 {
     public class Product
-    {   private string name { get; set; }
-        private double price { get; set; }
-        private double quantity { get; set; }
+    {   public string name { get; set; }
+        public double price { get; set; }
+        public double quantity { get; set; }
 
         public Product(string name,double price,double quantity)
         {
-            setName(name);
-            setPrice(price);
-            setQuantity(quantity);
-        }
-        public string getName()
-        {
-            return name;
-        }
-
-        public double getPrice()
-        {
-            return price;
-        }
-
-        public double getQuantity()
-        {
-            return quantity;
-        }
-
-        public void setName(string name)
-        {
             this.name = name;
+            this.price = price > 0 ? price : 0;
+            this.quantity = quantity > 0 ? quantity : 0;
         }
 
-        public void setPrice(double price)
-        {
-            this.price = price >= 0 ? price : 0;
-        }
-
-        public void setQuantity(double quantity)
-        {
-            this.quantity = quantity >= 0 ? quantity : 0;
-        }
-
-        public string toString()
+        public string ToString()
         {
             return $"name = {name}  price = {price}  quantity = {quantity}";
         }
